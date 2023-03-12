237ba28907a8fd02b3cd42d6fd8e06a66a112bf8
shahzaib1044 <127635002+shahzaib1044@users.noreply.github.com>
2023-03-12 15:15:12 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { CurrentUserContext } from './contexts/currentUserContext'
+import { AllUsersContext } from './contexts/allUsersContext';
+import { AllPostsContext } from './contexts/allPostsContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <AllUsersContext>
+      <CurrentUserContext>
+        <AllPostsContext>
+          <App />
+        </AllPostsContext>
+      </CurrentUserContext>
+    </AllUsersContext>
+  </Router>
+)
